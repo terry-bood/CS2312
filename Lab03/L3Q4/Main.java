44887208a9ca32a724a9ffc0fe6f45ffd355680d
terry-bood <spterrychan@gmail.com>
2026-09-22 17:12:09 +0800

    Add files via upload

---
+++ Lab03/L3Q4/Main.java
@@ -0,0 +1,121 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	
+	//Your task:
+	//Todo(1) the main() method --- see Main_GivenCode.txt
+	//Todo(2) the createTeams() method --- see Main_GivenCode.txt
+	//Todo(3) the decideTasks() method --- see Main_GivenCode.txt
+	public static void main(String [] args) throws FileNotFoundException
+	{	
+		System.out.print("Please input the file pathname: ");
+		Scanner in = new Scanner(System.in);
+		String filepathname = in.nextLine();
+		
+		Student[] students; //Define an array of students: Student[] students;
+		students = Student.createStudentListFromFile(filepathname);; //Call the given static method in the Student class for reading from file: Student.createStudentListFromFile(filepathname);
+		
+		System.out.println("Total number of students: " + students.length);
+		System.out.print("Enter the numberof teams: ");
+		int numOfTeams = in.nextInt();
+
+		if(students.length % numOfTeams != 0){
+			System.out.println("Wrong input - It is not a factor of " + students.length+".");
+			in.close();
+			return;
+		}
+
+		Team[] teams; //Define an array of teams: Team [] 
+		teams = createTeams(students,numOfTeams);//Call the private method (one of the 3 methods in Main.java) to create the teams for students: createTeams(students);
+		
+		//Print the grouping result:
+		System.out.println("Grouping result: ");				
+
+		for(int i=0;i<numOfTeams;i++){
+			System.out.println(teams[i].toString());
+		}
+
+		/*
+		System.out.println(teams[2].toString());
+		System.out.println(teams[0].toString());
+		System.out.println(teams[1].toString());
+		System.out.println(teams[3].toString());
+		System.out.println(teams[4].toString());
+		*/
+
+		System.out.print("\nEnter " + numOfTeams +" task names (e.g. \"Lab05 Lab06 Lab07 ..\"): ");
+		String[] taskname = new String[numOfTeams];
+		for(int i=0;i<numOfTeams;i++){
+			taskname[i] = in.next();
+		}
+		//Create the assignments
+		Assignment[] assignments; //Define an array of assignments: Assignment[] assignments 
+		assignments = decideTasks(teams, in,numOfTeams,taskname);//Call the private method (one of the 3 methods in Main.java) to input the tasks for each team: decideTasks(teams, in); 
+		
+		//Display sorted listing by tasks:
+		System.out.println("\nSorted listing by tasks: ");
+		// Assignment.printTaskTeam("Lab05", assignments);
+		// Assignment.printTaskTeam("Lab06", assignments);
+		// Assignment.printTaskTeam("Lab07", assignments);
+		// Assignment.printTaskTeam("Lab08", assignments);
+		// Assignment.printTaskTeam("Lab09", assignments);
+		for(int i=0;i<numOfTeams;i++){
+			Assignment.printTaskTeam(taskname[i], assignments);
+		}
+		
+		in.close();
+	}
+
+	private static Team[] createTeams(Student[] students, int numOfTeams) 
+	{
+		Team[] result; //Define an array of teams to store the result: Team[] result;
+		result = new Team[numOfTeams]; //Create the array: new Team[5];
+		/*
+		//Create the 5 teams
+		//Hint: "Arrays.copyOfRange(students, 0,6);" means to copy from position 0 inclusive to position 6 EXCLUSIVE
+		result[0]=new Team("Team A", Arrays.copyOfRange(students, 0,6)); //give team name and array of 6 students: "Team A", Arrays.copyOfRange(students, 0,6)
+		result[1]=new Team("Team B", Arrays.copyOfRange(students, 6,12)); //Create the second team : new Team("Team B", Arrays.copyOfRange(students, 6,12));
+		result[2]=new Team("Team C", Arrays.copyOfRange(students, 12,18));
+		result[3]=new Team("Team D", Arrays.copyOfRange(students, 18,24));
+		result[4]=new Team("Team E", Arrays.copyOfRange(students, 24,30));
+		*/
+		
+		int left =0;
+		int right = students.length/numOfTeams;
+		int index = right;
+		for(int i=0;i<numOfTeams;i++){
+			char teamLetter = (char)('A' + i);
+			String teamS = "Team " + teamLetter;
+			result[i] = new Team(teamS, Arrays.copyOfRange(students, left,right));
+			left = right;
+			right += index;
+		}
+		
+		return result; //Return the result: return result;
+	}
+
+	//assignment of lab tasks to teams based on user input
+	private static Assignment[] decideTasks(Team[] teams, Scanner in,int numOfTeams, String[] name)
+	{
+		Assignment[] assignments; //Define an array of assignments to store the result: Assignment[] assignments;
+		assignments = new Assignment[numOfTeams]; //Create the array: new Assignment[5];
+
+		System.out.print("\nEnter tasks for the teams (");
+		for(int i=0;i<numOfTeams;i++){
+			System.out.print(name[i]);
+			if(i>0){System.out.print(",");}
+			if(i==numOfTeams-1){
+				System.out.println("):");
+			}
+		}
+		for (int i=0; i<numOfTeams; i++)
+		{
+			System.out.print(teams[i].getName() + ": "); //Prompt for a team: teams[i].getName()
+			String taskName = in.next(); //Get user input of the task name from keyboard: in.next();
+			assignments[i] = new Assignment(teams[i], new Task(taskName)); //Create the assignment for:  teams[i], new Task(taskName)
+		}
+		return assignments; //Return the result: return assignments;
+	}
+
+}
